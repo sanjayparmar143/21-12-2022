d00b39540624e3142c63fba4acda5112084cd2c3
sanjayparmar143 <118906048+sanjayparmar143@users.noreply.github.com>
2022-12-22 18:43:51 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+main(){
+	
+	char s;
+	
+	cout<<"enter value :- ";
+	cin>>s;
+	
+	if(s>='0' && s<='9'){
+		
+		cout<<"it's Numreric";
+	}
+	else{
+	
+	
+	cout<<"it's Not Numeric";	
+	
+	}
+	
+}
+	
